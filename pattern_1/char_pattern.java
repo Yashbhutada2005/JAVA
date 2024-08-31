129897f7e53bb0b338e34c9fabbb81192a954d3a
Yashbhutada2005 <yashbhutada05@gmail.com>
2024-08-31 20:39:47 +0530

    .

---
+++ pattern_1/char_pattern.java
@@ -0,0 +1,19 @@
+// print a charcter pattern
+
+package pattern_1;
+
+public class char_pattern {
+    public static void main(String[] args) {
+        
+    int n =7 ;
+    char ch = 'A';
+
+    for(int i = 1; i<=n; i++) {
+        for(int j = 1; j<=i ; j++) {
+            System.out.print(ch);
+            ch++ ;
+        }
+        System.out.println();
+    }
+}
+}
---
+++ pattern_1/half_pyramid_pattern.java
@@ -0,0 +1,15 @@
+// print half pyramid pattern
+
+package pattern_1;
+
+public class half_pyramid_pattern {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++) {
+            for(int j = 1; j <= i ; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        
+    }
+}
