7e17e1b0cdf63998b995533e41b4bd9545dbbec0
Yashbhutada2005 <yashbhutada05@gmail.com>
2024-09-02 19:03:29 +0530

    .

---
+++ function_methods/basics.java
@@ -0,0 +1,14 @@
+package function_methods;
+
+public class basics {
+    public static void printYashBhutada() {
+        System.out.println("YashBhutada");
+        System.out.println("YashBhutada");
+        System.out.println("YashBhutada");
+        System.out.println("YashBhutada");
+        return ; 
+    }
+    public static void main(String[] args) {
+        printYashBhutada();   // function call
+    }
+}
---
+++ function_methods/swap.java
@@ -0,0 +1,16 @@
+package function_methods;
+
+public class swap {
+    public static void main(String[] args) {
+        int a =12 ;
+        int b =23 ;
+
+        // swap a number by call a value
+        int temp = a;
+        a = b ;
+        b = temp ;
+
+        System.out.println("a = "+ a);
+        System.out.println("b = "+ b);
+    }
+}
