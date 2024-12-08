3fa2d27aee1b8bec554bc2e1a71e48c4c90154fc
Yashbhutada2005 <yashbhutada05@gmail.com>
2024-12-08 18:43:44 +0530

    Create problem2.java

---
+++ practice_set_1/problem2.java
@@ -0,0 +1,17 @@
+//greet with taking input the name
+
+package practice_set_1;
+
+import java.util.*;
+public class problem2 {
+    public static void main(String[] args) {
+        System.out.println("Enter name :");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+
+        System.out.println("Hi "+name +" have a good day" );
+        
+        
+    }
+    
+}
