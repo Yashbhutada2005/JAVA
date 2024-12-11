f5d70b999db19919a323df37f7c91cbd5d37b2d9
Yashbhutada2005 <yashbhutada05@gmail.com>
2024-12-11 18:10:45 +0530

    Create problem1.java

---
+++ String/problem1.java
@@ -0,0 +1,11 @@
+// replace a name by yash
+
+package String;
+
+public class problem1 {
+    public static void main(String[] args) {
+        
+        String letter = "Dear name , how are you";
+        System.out.println(letter.replace("name", "yash"));
+    }
+}
