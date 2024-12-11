649eead4d2bebe62bb9deed54bc6243292790554
Yashbhutada2005 <yashbhutada05@gmail.com>
2024-12-11 18:18:56 +0530

    Create problem2.java

---
+++ String/problem2.java
@@ -0,0 +1,11 @@
+package String;
+
+public class problem2 {
+    public static void main(String[] args) {
+        String myString = "yash   bhutada    girish bhutada";
+        
+
+        System.out.println(myString.indexOf("   "));
+        System.out.println(myString.indexOf("    "));
+    }
+}
