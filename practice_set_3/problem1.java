4e20c020852d719e0d6876cc201d6dfaadc705f1
Yashbhutada2005 <yashbhutada05@gmail.com>
2024-12-12 17:39:12 +0530

    Create problem1.java

---
+++ practice_set_3/problem1.java
@@ -0,0 +1,21 @@
+// sum first n even number 
+
+package practice_set_3;
+import java.util.Scanner;
+public class problem1 {
+    public static void main(String[] args) {
+        
+        System.out.println("Enter number :");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = n * (n+1) / 2;
+
+        for(int i = 1; i <= n ; i++) {
+            sum += i;
+        }
+        
+            System.out.println(sum);
+        }
+    }
+
