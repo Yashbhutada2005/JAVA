3101251dc4d8d37084f4a87808781197a89b07b4
Yashbhutada2005 <yashbhutada05@gmail.com>
2024-12-14 18:03:21 +0530

    Create problem1.java

---
+++ practice_set_4/problem1.java
@@ -0,0 +1,20 @@
+// SUM OF THE ARRAY
+
+package practice_set_4;
+
+public class problem1 {
+    public static void main(String[] args) {
+      
+        int arr[] = {12,34,56,7,89,98};
+        int sum = 0;
+
+        for(int element:arr) {
+            sum = sum + element;
+
+        }
+        System.out.println("the sum of the array is " + sum);
+            
+        }
+        
+    }
+
