020c1430d51a76cd7ecb9cfd81c6c6738c0d6ee6
Yashbhutada2005 <yashbhutada05@gmail.com>
2024-12-18 20:05:45 +0530

    Create isPrime.java

---
+++ function_methods/isPrime.java
@@ -0,0 +1,24 @@
+// CHECK WHETHER THE GIVEN NUMBER IS PRIME OR NOT
+
+package function_methods;
+    import java.util.*;
+    public class isPrime {
+    
+        public static boolean isPrime(int n){
+
+            boolean isPrime = true;
+            if(n % 2 == 0) {
+                isPrime = false;
+            }
+            return isPrime;
+        }
+
+        public static void main(String[] args) {
+            
+            System.out.println("Enter Number :");
+            Scanner sc = new Scanner(System.in);
+            int num = sc.nextInt();
+
+            System.out.println(isPrime(num));
+        }
+}
