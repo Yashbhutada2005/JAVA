5035da3ff0a9b36dbbc4927bcab73443a779fc78
Yashbhutada2005 <yashbhutada05@gmail.com>
2025-01-23 11:34:24 +0530

    .

---
+++ OOP_practice_set/problem9.java
@@ -0,0 +1,32 @@
+// ABSTRACT CLASS
+
+package OOP_practice_set;
+
+abstract class Animal {
+   abstract void sound();
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog Barks!!");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void sound(){
+        System.out.println("Cat Meows!!");
+    }
+}
+public class problem9 {
+    public static void main(String[] args) {
+        
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        dog.sound();
+        cat.sound();
+    }
+    
+}
