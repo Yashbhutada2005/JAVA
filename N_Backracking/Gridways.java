28bfa055b5569889cbed8621380847692f9e5cc3
Yashbhutada2005 <yashbhutada05@gmail.com>
2025-02-28 15:15:13 +0530

    .

---
+++ N_Backracking/Gridways.java
@@ -0,0 +1,23 @@
+package N_Backracking;
+
+public class Gridways {
+
+    public static int Gridways(int i , int j , int n , int m){
+        // base case 
+        if(i == n-1 && j == m-1){
+            return 1;
+        }
+        else if(i == n || j == n){
+            return 0;
+        }
+
+        int w1 = Gridways(i+1, j, n, m);
+        int w2 = Gridways(i, j+1, n, m);
+        return w1 + w2;
+    }
+    
+    public static void main(String[] args) {
+        int n = 4 , m = 4;
+        System.out.println(Gridways(0, 0, n, m));
+    }
+}
