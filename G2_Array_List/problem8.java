bffb5c83a6fea942c2d0fc0c3dcf3b46785b4439
Yashbhutada2005 <yashbhutada05@gmail.com>
2025-03-17 11:20:51 +0530

    .

---
+++ G2_Array_List/problem8.java
@@ -0,0 +1,19 @@
+// pair sum 2 - target value
+
+package G2_Array_List;
+import java.util.*;
+
+public class problem8 {
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+    }
+    
+}
