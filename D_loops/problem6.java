59d6b5799c25f1a200a34ca30961f8883f489f0d
Yash Bhutada <yashbhutada05@gmail.com>
2026-01-28 15:28:02 +0530

    .

---
+++ D_loops/problem6.java
@@ -0,0 +1,26 @@
+package D_loops;
+
+import java.util.*;
+
+public class problem6 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int num = sc.nextInt();
+        int count = 0;
+
+        for(int i=1; i<=num; i++){
+            if(num % i == 0){
+                count++;
+            }
+        }
+        if(count == 2){
+            System.out.println("Number is Prime");
+        }
+        else {
+            System.out.println("Number is not Prime");
+        }
+        sc.close();
+    }
+}
