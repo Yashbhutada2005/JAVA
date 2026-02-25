f4b6916f42fd6219e883e9e138b3a619153d85c9
Yash Bhutada <yashbhutada05@gmail.com>
2026-02-25 16:25:32 +0530

    Update problem_7.java

--- G_Array/problem_7.java
+++ G_Array/problem_7.java
@@ -5,16 +5,14 @@ package G_Array;
 public class problem_7 {
     public static void main(String[] args) {
         int[] arr = {4,6,3,5,8,2};
-        int ans = 0;
         int target = 7;
 
         for(int i = 0; i < arr.length ; i++) {
             for(int j = 0; j < arr.length ; j++) {
             if(arr[i] + arr[j] == target) {
-                ans++;
+                System.out.println(arr[i] + " + " + arr[j] + " = " + target);
             }
         }
     }
-    System.out.println(ans);
 }
 }
