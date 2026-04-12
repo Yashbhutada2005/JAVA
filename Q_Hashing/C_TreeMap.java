ab96be6c3643421f043bf95d61c825929595d7db
Yash Bhutada <yashbhutada05@gmail.com>
2026-04-12 12:12:23 +0530

    Create C_TreeMap.java

---
+++ Q_Hashing/C_TreeMap.java
@@ -0,0 +1,16 @@
+package Q_Hashing;
+
+import java.util.TreeMap;
+
+public class C_TreeMap {
+    public static void main(String[] args) {
+        
+        TreeMap<String , Integer> tm = new TreeMap<>();
+        
+        tm.put("C" , 87);
+        tm.put("A" , 23);
+        tm.put("B" , 45);
+
+        System.out.println(tm);
+    }
+}
