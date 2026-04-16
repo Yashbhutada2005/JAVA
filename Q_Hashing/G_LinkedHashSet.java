cdbf31b60d8f0e82e2ffc4d0f726913ad7036b95
Yash Bhutada <yashbhutada05@gmail.com>
2026-04-16 09:48:42 +0530

    Create G_LinkedHashSet.java

---
+++ Q_Hashing/G_LinkedHashSet.java
@@ -0,0 +1,17 @@
+package Q_Hashing;
+
+import java.util.LinkedHashSet;
+
+public class G_LinkedHashSet {
+    public static void main(String[] args) {
+        
+        LinkedHashSet<String> set = new LinkedHashSet<>();
+
+        set.add("A");
+        set.add("B");
+        set.add("C");
+        set.add("D");
+
+        System.out.println(set);
+    }
+}
