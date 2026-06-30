c25bf149493d9c7a48f381bbbb9e4eb4f61aa87e
Yash Bhutada <yashbhutada05@gmail.com>
2026-06-30 18:54:27 +0530

    Update arithmetic.java

--- B_operators/arithmetic.java
+++ B_operators/arithmetic.java
@@ -4,10 +4,11 @@ public class arithmetic {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
+        int c = 22;
 
-        int add = a+b;
-        int sub = a-b;
-        int mul = a*b;
+        int add = a+b+c;
+        int sub = a-b-c;
+        int mul = a*b*c;
         int div = b/a;
         int mod = a%b;
 
