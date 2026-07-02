09ab2379066bd879386a269e71cbdecffd49e712
Yash Bhutada <yashbhutada05@gmail.com>
2026-07-02 08:53:22 +0530

    Create Problem10.java

---
+++ A_Basics/Problem10.java
@@ -0,0 +1,12 @@
+ // multiplication table
+ package A_Basics;
+
+public class Problem10 {
+    
+    public static void main(String[] args) {
+        
+        for(int i = 1; i <= 10; i++){
+            System.out.println("5 x " + i + " = " + (5*i));
+        }
+    }
+}
